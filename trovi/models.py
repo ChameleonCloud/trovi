5c513adb73d620b3a6909a9472d3764f81cc2671
Adam Cooper <adamcooper@uchicago.edu>
2021-10-25 17:22:51 -0500

    Clean up models

--- trovi/models.py
+++ trovi/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from fields import URNField
+from settings import ARTIFACT_SHARING_MAX_REPRO_REQUESTS
 
 
 class Artifact(models.Model):
@@ -18,7 +19,7 @@ class Artifact(models.Model):
     uuid = models.UUIDField(primary_key=True)
 
     # Descriptive information
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=70)
     short_description = models.CharField(max_length=70, blank=True, null=True)
     long_description = models.TextField(max_length=5000)
 
@@ -33,7 +34,10 @@ class Artifact(models.Model):
     is_reproducible = models.BooleanField(default=False)
     repro_requests = models.IntegerField(
         default=0,
-        validators=[validators.MinValueValidator(0), validators.MaxValueValidator(10)],
+        validators=[
+            validators.MinValueValidator(0),
+            validators.MaxValueValidator(ARTIFACT_SHARING_MAX_REPRO_REQUESTS),
+        ],
     )
     repro_access_hours = models.IntegerField(null=True)
 
@@ -50,12 +54,17 @@ class ArtifactVersion(models.Model):
     id = models.IntegerField(primary_key=True)
     slug = models.SlugField(primary_key=True, max_length=12)
 
-    artifact_id = models.ForeignKey(
-        Artifact, models.CASCADE, related_name="artifact_version"
-    )
+    artifact = models.ForeignKey(Artifact, models.CASCADE, related_name="versions")
     created_at = models.DateTimeField(auto_now_add=True)
     contents_urn = URNField(max_length=255)
-    access_count = models.IntegerField(default=0)
+
+    @property
+    def access_count(self) -> int:
+        """
+        Shortcut for determining how many times an artifact version has been launched
+        :return: The number of LAUNCH events for this artifact version
+        """
+        return self.events.filter(event_type=ArtifactEvent.EventType.LAUNCH).count()
 
 
 class ArtifactEvent(models.Model):
@@ -68,11 +77,14 @@ class ArtifactEvent(models.Model):
 
     # The artifact version this event is for
     artifact_version = models.ForeignKey(
-        ArtifactVersion, models.CASCADE, related_name="artifact_event"
+        ArtifactVersion, models.CASCADE, related_name="events"
     )
 
     # The type of event
-    event_type = models.CharField(max_length=6, choices=EventType.choices)
+    event_type = models.CharField(
+        max_length=max(len(key) for key, _ in EventType.choices),
+        choices=EventType.choices,
+    )
     # The user who initiated the event
     event_origin = URNField(max_length=255, null=True)
 
@@ -83,17 +95,17 @@ class ArtifactEvent(models.Model):
 class ArtifactTag(models.Model):
     """Represents a searchable and sortable tag which can be applied to any artifact"""
 
-    artifact_id = models.ForeignKey(
-        Artifact, models.CASCADE, related_name="artifact_tag"
+    artifact = models.ForeignKey(
+        Artifact, models.CASCADE, related_name="tags"
     )
-    tag = models.CharField(max_length=32)
+    tag = models.CharField(max_length=32, unique=True)
 
 
 class ArtifactAuthor(models.Model):
     """Represents an author of an artifact"""
 
-    artifact_id = models.ForeignKey(
-        Artifact, models.CASCADE, related_name="artifact_author"
+    artifact = models.ForeignKey(
+        Artifact, models.CASCADE, related_name="author"
     )
     full_name = models.CharField(max_length=200)
     affiliation = models.CharField(max_length=200, blank=True, null=True)
@@ -103,7 +115,7 @@ class ArtifactAuthor(models.Model):
 class ArtifactProject(models.Model):
     """Represents the project associated with an artifact"""
 
-    artifact_id = models.ForeignKey(
-        Artifact, models.CASCADE, related_name="artifact_project"
+    artifact = models.ForeignKey(
+        Artifact, models.CASCADE, related_name="project"
     )
-    urn = URNField(max_length=255)
+    urn = URNField(max_length=255, unique=True)
