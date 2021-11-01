323261dbdecbc861e876662c45c4ac97fa527a62
Adam Cooper <adamcooper@uchicago.edu>
2021-11-01 11:07:25 -0500

    Remove another relative path import

--- trovi/api/urls.py
+++ trovi/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-import api.views
+from trovi.api import views
 
 urlpatterns = [
-    path("", api.views.ListArtifact.as_view()),
-    path("<uuid:artifact_uuid>", api.views.GetArtifact.as_view()),
+    path("", views.ListArtifact.as_view()),
+    path("<uuid:artifact_uuid>", views.GetArtifact.as_view()),
 ]
