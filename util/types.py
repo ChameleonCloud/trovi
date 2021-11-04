7f71c3c86a069f89f957a2ffdd1ad1a049f10316
Adam Cooper <adamcooper@uchicago.edu>
2021-11-04 13:43:34 -0500

    Patch custom types

--- util/types.py
+++ util/types.py
@@ -1,27 +1,25 @@
+from typing import Union, Protocol, Type, Dict
+
 from django.db import models
-from typing import TYPE_CHECKING, Union, Protocol, Type, Dict, Any
-
-if TYPE_CHECKING:
-
-    class JSONArray(Protocol):
-        __class__: Type[list["JSON"]]
-
-    class JSONObject(Protocol):
-        __class__: Type[dict[str, "JSON"]]
-
-    class JSONPrimitive(Protocol):
-        __class__: Type[Union[None, float, int, str]]
-
-    JSON = Union[JSONPrimitive, JSONArray, JSONObject]
-
-    class APISerializable(Protocol):
-        __class__: Type[Union[models.Field, models.Manager, "APIFormat"]]
-
-    APIFormat = Dict[str, APISerializable]
 
 
-else:
-    JSONPrimitive = Any
-    JSON = dict
-    APISerializable = Any
-    APIFormat = dict
+class JSONArray(Protocol):
+    __class__: Type[list["JSON"]]
+
+
+class JSONObject(Protocol):
+    __class__: Type[dict[str, "JSON"]]
+
+
+class JSONPrimitive(Protocol):
+    __class__: Type[Union[None, float, int, str]]
+
+
+JSON = Union[JSONPrimitive, JSONArray, JSONObject]
+
+
+class APISerializable(Protocol):
+    __class__: Type[Union[models.Field, models.Manager, "APIObject"]]
+
+
+APIObject = Dict[str, Union[APISerializable, JSON]]
