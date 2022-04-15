00ca38e1b7e9a1f7be8ede10f9b1fe0e984b9e32
Mark Powers <markpowers@uchicago.edu>
2022-04-15 09:59:15 -0500

    Fix github http link parsing

--- trovi/storage/backends/git.py
+++ trovi/storage/backends/git.py
@@ -1,11 +1,14 @@
 from datetime import datetime
 from django.conf import settings
+import logging
 from typing import Hashable, Optional
 from urllib.parse import urljoin
 
 from trovi.storage.backends.base import StorageBackend
 from trovi.storage.links.http import HttpDownloadLink
 from trovi.storage.links.git import GitDownloadLink
+
+LOG = logging.getLogger(__name__)
 
 class GitBackend(StorageBackend):
     """
@@ -18,9 +21,7 @@ class GitBackend(StorageBackend):
         content_id: Hashable = None,
     ):
         self.name = name
-
-
-        parts = content_type.split("@")
+        parts = content_id.split("@")
         self.remote_url = parts[0]
         if len(parts) > 1:
             self.ref = parts[1]
@@ -43,6 +44,8 @@ class GitBackend(StorageBackend):
             return None
 
         github_base_url = self.remote_url
+        if github_base_url.endswith(".git"):
+            github_base_url = github_base_url[:-4]
         if not github_base_url.endswith("/"):
             github_base_url += "/"
         url = urljoin(github_base_url, f"archive/{self.ref}.zip")
