7a7720255d5129140d2b29728a96d86c2cbf506a
Adam Cooper <adamcooper@uchicago.edu>
2022-06-08 17:40:00 -0500

    Log any request which results in an error

--- trovi/common/handlers.py
+++ trovi/common/handlers.py
@@ -1,7 +1,13 @@
+import logging
+
+from rest_framework import status
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import exception_handler
 
 from trovi.common.exceptions import InvalidToken
+
+LOG = logging.getLogger(__name__)
 
 
 def trovi_exception_handler(exc: Exception, context: dict) -> Response:
@@ -16,5 +22,13 @@ def trovi_exception_handler(exc: Exception, context: dict) -> Response:
         }
         if exc.error_uri:
             response.data["error_uri"] = exc.error_uri
+
+    request = context["request"]
+    status_code = response.status_code
+    log_msg = f"{request.get_full_path()} {exc} {response.status_code}"
+    if status_code >= status.HTTP_500_INTERNAL_SERVER_ERROR:
+        LOG.error(log_msg)
+    else:
+        LOG.warning(log_msg)
 
     return response
